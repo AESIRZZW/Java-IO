02b5c2bc662d8fc45bfb2f16487473999a5a1df4
aesir_glb <aesir1995@outlook.com>
2019-04-15 23:13:32 +0800

    [Version:1.0]

---
+++ src/com/aesirtech/learning/java/io/nodestream/CharStreamDemo.java
@@ -0,0 +1,52 @@
+package com.aesirtech.learning.java.io.nodestream;
+
+import java.io.*;
+
+/**
+ * @ProjectName: Java IO
+ * @Description: A Simple Demonstration of Char Stream Reading & Writing
+ * @Author: Aesir
+ * @Date: 2019/4/15 22:07
+ */
+public class CharStreamDemo {
+    public static void main(String[] args) {
+        File sourceFile = new File("resource/The Road Not Taken.txt");
+        File targetFile = new File("resource/The Road Not Taken.bak");
+        FileReader fileReader = null;
+        FileWriter fileWriter = null;
+        try {
+            fileReader = new FileReader(sourceFile);
+            fileWriter = new FileWriter(targetFile);
+
+            char[] cbuf = new char[10];
+            // Record the number of chars read into cbuf each time.
+            int len;
+            while((len = fileReader.read(cbuf)) != -1){
+                fileWriter.write(cbuf,0,len);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            /*
+             * Write separately to ensure that each stream is closed
+             * even if the other stream has an exception.
+             */
+            try {
+                if (fileReader != null) {
+                    fileReader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (fileWriter != null) {
+                    fileWriter.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
